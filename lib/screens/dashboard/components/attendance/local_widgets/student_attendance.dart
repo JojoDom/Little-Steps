6e0e400fb20ec5a34333025936e1e922c6c330cb
JojoDom <adwoa_dadson@yahoo.com>
2022-10-28 15:21:21 +0000

    added date filter

--- lib/screens/dashboard/components/attendance/local_widgets/student_attendance.dart
+++ lib/screens/dashboard/components/attendance/local_widgets/student_attendance.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:little_steps/controllers/attendance_controller.dart';
 import 'package:little_steps/screens/dashboard/components/attendance/local_widgets/attendance_list_item.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:intl/intl.dart';
 
 class StudentAttendance extends StatefulWidget {
   const StudentAttendance({Key? key}) : super(key: key);
@@ -33,89 +34,105 @@ class _StudentAttendanceState extends State<StudentAttendance> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => attendanceController.presentStudents.isEmpty
-        ? SmartRefresher(
-            controller: _refreshController,
-            enablePullDown: true,
-            enablePullUp: false,
-            header: const WaterDropHeader(),
-            onRefresh: _onRefresh,
-            child: ListView(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.1,
-                ),
-                Center(child: Image.asset('assets/images/no_result.png')),
-                Center(
-                  child: Text(
-                    'No student has checked in',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodySmall!
-                        .copyWith(fontSize: 14),
+    return Obx(() => Column(
+          children: [
+            const SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.4,
+                  top: 20,
+                  right: 30),
+              child: TextFormField(
+                onTap: (() async {
+                  DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1950),
+                      lastDate: DateTime(2100));
+                  if (pickedDate != null) {
+                    attendanceController.checkedInStudents(
+                        date: DateFormat('y-M-d').format(pickedDate));
+                    dateController.text =
+                        DateFormat('d MMM, y').format(pickedDate);
+                  } else {}
+                }),
+                controller: dateController,
+                readOnly: true,
+                decoration: InputDecoration(
+                  hintText: 'Select date ',
+                  suffixIcon: Icon(
+                    Icons.calendar_month,
+                    color: Theme.of(context).primaryColor,
                   ),
-                )
-              ],
-            ),
-          )
-        : Column(
-            children: [
-              const SizedBox(height: 20),
-              Padding(
-                padding: EdgeInsets.only(
-                    left: MediaQuery.of(context).size.width * 0.5,
-                    top: 20,
-                    right: 30),
-                child: TextFormField(
-                  controller: dateController,
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    hintText: 'Select date ',
-                    suffixIcon: Icon(
-                      Icons.calendar_month,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(0),
-                        borderSide: const BorderSide(
-                          color: Color(0xFFE7979C),
-                        )),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(0),
-                        borderSide: BorderSide(
-                          color: Colors.grey.shade300,
-                        )),
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 20),
-                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      borderSide: const BorderSide(
+                        color: Color(0xFFE7979C),
+                      )),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade300,
+                      )),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
                 ),
               ),
-              attendanceController.isCheckedInStudents.isFalse
-                  ? Expanded(
-                      child: SmartRefresher(
-                        controller: refreshController,
-                        enablePullDown: true,
-                        enablePullUp: false,
-                        header: const WaterDropHeader(),
-                        onRefresh: _onRefresh,
-                        child: ListView.separated(
-                            physics: const AlwaysScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            itemBuilder: (context, index) => AttendanceListItem(
-                                attendance: attendanceController
-                                    .presentStudents[index]),
-                            separatorBuilder: (context, index) => const Divider(
-                                  height: 0.7,
-                                  color: Color(0xFF999999),
-                                ),
-                            itemCount:
-                                attendanceController.presentStudents.length),
-                      ),
-                    )
-                  : const LinearProgressIndicator(
-                      color: Colors.green,
+            ),
+              attendanceController.presentStudents.isEmpty
+                    ? Expanded(
+                        child: SmartRefresher(
+                           controller: _refreshController,
+                      enablePullDown: true,
+                      enablePullUp: false,
+                      header: const WaterDropHeader(),
+                      onRefresh: _onRefresh,
+                          child: ListView(
+                          // mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.1,
+                            ),
+                            Center(
+                                child:
+                                    Image.asset('assets/images/no_result.png')),
+                            Center(
+                              child: Text(
+                                'No student found',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall!
+                                    .copyWith(fontSize: 14),
+                              ),
+                            )
+                          ],
+                                              ),
+                        )):
+            attendanceController.isCheckedInStudents.isFalse
+                ? Expanded(
+                    child: SmartRefresher(
+                      controller: refreshController,
+                      enablePullDown: true,
+                      enablePullUp: false,
+                      header: const WaterDropHeader(),
+                      onRefresh: _onRefresh,
+                      child: ListView.separated(
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) => AttendanceListItem(
+                              attendance:
+                                  attendanceController.presentStudents[index]),
+                          separatorBuilder: (context, index) => const Divider(
+                                height: 0.7,
+                                color: Color(0xFF999999),
+                              ),
+                          itemCount:
+                              attendanceController.presentStudents.length),
                     ),
-            ],
-          ));
+                  )
+                    : const LinearProgressIndicator(
+                        color: Colors.green,
+                      ),
+          ],
+        ));
   }
 }
