3e56b1a53c8e5daff5ef65cadeb150323c942e7a
JojoDom <adwoa_dadson@yahoo.com>
2024-01-04 08:43:08 +0000

    using getx for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Little Steps',
       theme: Themes().lightTheme,
       darkTheme: Themes().darkTheme,
--- lib/screens/home/bloc/home_bloc.dart
+++ lib/screens/home/bloc/home_bloc.dart
@@ -2,8 +2,16 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:little_steps/screens/students/all_students.dart';
+import 'package:little_steps/screens/teachers/local_widgets/all_teachers.dart';
+import 'package:little_steps/screens/teachers/teachers.dart';
 import 'package:logger/logger.dart';
 import 'package:meta/meta.dart';
+
+import '../../attendance/attendance.dart';
+import '../local_widgets/scan_student_qr_code.dart';
+import '../local_widgets/scan_teacher_qr_code.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
@@ -16,30 +24,30 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   FutureOr<void> homeMenuItemsOnTapEvent(
       HomeMenuItemsOnTapEvent event, Emitter<HomeState> emit) {
     Logger().i(event.tag);
-    // switch (event.tag) {
-    //       case 'checkInStudent':
-    //         Get.to(const ScanStudentQrCode(isCheckIn: true));
-    //         break;
-    //       case 'checkOutStudent':
-    //         Get.to(const ScanStudentQrCode(isCheckIn: false));
-    //         break;
-    //       case 'checkInTeacher':
-    //         Get.to(const ScanTeacherQrCode(isCheckIn: true));
-    //         break;
-    //       case 'checkOutTeacher':
-    //         Get.to(const ScanTeacherQrCode(isCheckIn: false));
-    //         break;
-    //       case 'studentAttendance':
-    //         Get.to(const Attendance());
-    //         break;
-    //       case 'teachersAttendance':
-    //         Get.to(const Teachers());
-    //         break;
-    //       case 'allStudents':
-    //         Get.to(const AllStudents());
-    //         break;
-    //       case 'allTeachers':
-    //         Get.to(const AllTeachers());
-    //     }
+    switch (event.tag) {
+          case 'checkInStudent':
+            Get.to(const ScanStudentQrCode(isCheckIn: true));
+            break;
+          case 'checkOutStudent':
+            Get.to(const ScanStudentQrCode(isCheckIn: false));
+            break;
+          case 'checkInTeacher':
+            Get.to(const ScanTeacherQrCode(isCheckIn: true));
+            break;
+          case 'checkOutTeacher':
+            Get.to(const ScanTeacherQrCode(isCheckIn: false));
+            break;
+          case 'studentAttendance':
+            Get.to(const Attendance());
+            break;
+          case 'teachersAttendance':
+            Get.to(const Teachers());
+            break;
+          case 'allStudents':
+            Get.to(const AllStudents());
+            break;
+          case 'allTeachers':
+            Get.to(const AllTeachers());
+        }
   }
 }
